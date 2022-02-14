39eb9a4be8398c5ef913976be303fd864c71b81b
C4l1b4n <66850821+C4l1b4n@users.noreply.github.com>
2022-02-14 16:54:46 +0100

    Add files via upload
    Grey scale augmenter

--- Augmenters/AugmenterGrey.py
+++ Augmenters/AugmenterGrey.py
@@ -7,16 +7,16 @@ from scipy import ndimage
 class AugmenterGrey(Augmenter):
 		
 	def __init__(self):
-		self.__transformation_matrix__: np.ndarray = np.array([[1, 0, 0, 0], 
-        	                      			       		       [0, 1, 0, 0], 
-        	                      			       		       [0, 0, 1, 0],
-        	                                       			   [0, 0, 0, 1]])
+		self.__transformation_matrix__: np.ndarray = np.array([[1,  0,  0, 0], 
+        	                      			       		[0, 1,  0, 0], 
+        	                      			       		[0,  0,  1, 0],
+        	                                       			[0,  0,  0, 1]])
 
 	def transform(self, image: Image):
-		self.__transformation_matrix__: np.ndarray = np.array([[1, 0, 0, 0], 
-        	                      			      		       [0, 1, 0, 0], 
-        	                      			       		       [0, 0, 0, 0],
-        	                                       			   [0, 0, 0, 1]])
+		self.__transformation_matrix__: np.ndarray = np.array([[1,  0,  0, 0], 
+        	                      			      		[0, 1,  0, 0], 
+        	                      			       		[0,  0,  0, 0],
+        	                                       			[0,  0,  0, 1]])
 
 		return ndimage.affine_transform(input  = self.get_array_from_image(image),
                  	                    matrix = self.__transformation_matrix__)
