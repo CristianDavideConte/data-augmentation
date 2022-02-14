5e52f96040f45f8084e019aad871cb4a2ce0d3ae
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-02-14 20:01:17 +0100

    Create AugmenterGrayscale.py

---
+++ Augmenters/AugmenterGrayscale.py
@@ -0,0 +1,25 @@
+from Augmenter import Augmenter
+import numpy as np
+
+from PIL import Image
+from scipy import ndimage
+
+class AugmenterGrayscale(Augmenter):
+		
+	def __init__(self):
+		self.__transformation_matrix__: np.ndarray = np.array([[1, 0, 0, 0], 
+															   [0, 1, 0, 0], 
+        	                      			       			   [0, 0, 1, 0],
+															   [0, 0, 0, 1]])
+
+	def transform(self, image: Image):
+		self.__transformation_matrix__: np.ndarray = np.array([[1, 0, 0, 0], 
+															   [0, 1, 0, 0], 
+															   [0, 0, 0, 0],
+															   [0, 0, 0, 1]])
+
+		return ndimage.affine_transform(input  = self.get_array_from_image(image),
+                 	                    matrix = self.__transformation_matrix__)
+    
+	def get_transformed_label(self, center_x: float, center_y: float, width: float, height: float):       	
+		return center_x, center_y, width, height
