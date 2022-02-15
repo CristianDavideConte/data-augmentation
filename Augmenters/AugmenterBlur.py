aa74846af8b86c062e25c017678f5460bb70d5c7
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-02-15 18:06:51 +0100

    Update AugmenterBlur.py

--- Augmenters/AugmenterBlur.py
+++ Augmenters/AugmenterBlur.py
@@ -7,15 +7,14 @@ from scipy.ndimage.filters import gaussian_filter
 
 class AugmenterGrey(Augmenter):
 	def __init__(self, blur: float):
-		self.__blur__ = blur
-		self.__transformation_matrix__: np.ndarray = np.array([[1,  0,  0, 0], 
-        	                      			       		[0, 1,  0, 0], 
-        	                      			       		[0,  0,  1, 0],
-        	                                       			[0,  0,  0, 1]])
+		self.__blur__ = abs(blur)
+		self.__transformation_matrix__: np.ndarray = np.array([[1, 0, 0, 0], 
+        	                      			       			   [0, 1, 0, 0], 
+        	                      			       			   [0, 0, 1, 0],
+        	                                       			   [0, 0, 0, 1]])
 
 	def transform(self, image: Image):
-		image = gaussian_filter(image,(abs(self.__blur__),abs(self.__blur__),0))
-		return image
-    
+		return gaussian_filter(image, (self.__blur__, self.__blur__, 0))
+		
 	def get_transformed_label(self, center_x: float, center_y: float, width: float, height: float):       	
 		return center_x, center_y, width, height
