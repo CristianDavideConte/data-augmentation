043232b43094b9dc7b41cdb4157d219656bb62b7
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-02-16 10:21:13 +0100

    Update AugmenterNoise.py

--- Augmenters/AugmenterNoise.py
+++ Augmenters/AugmenterNoise.py
@@ -6,16 +6,16 @@ from skimage.util import random_noise
 
 class AugmenterGrey(Augmenter):
 		
-	def __init__(self, noise: float):
-		self.__noise__ = abs(noise)
-		self.__transformation_matrix__: np.ndarray = np.array([[1, 0, 0, 0], 
+    def __init__(self, noise: float):
+        self.__noise__ = abs(noise)
+        self.__transformation_matrix__: np.ndarray = np.array([[1, 0, 0, 0], 
                                                                [0, 1, 0, 0], 
                                                                [0, 0, 1, 0],
                                                                [0, 0, 0, 1]])
 
-	def transform(self, image: Image):
-		image = random_noise(image, mode = "gaussian", var = self.__noise__)
-		return (255 * image).astype(np.uint8)
+    def transform(self, image: Image):
+        image = random_noise(image, mode = "gaussian", var = self.__noise__)
+        return (255 * image).astype(np.uint8)
 		
-	def get_transformed_label(self, center_x: float, center_y: float, width: float, height: float):       	
-		return center_x, center_y, width, height
+    def get_transformed_label(self, center_x: float, center_y: float, width: float, height: float):       	
+        return center_x, center_y, width, height
